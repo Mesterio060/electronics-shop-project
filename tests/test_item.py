1a622431bf544e6dc7c404482b2241cfb5acf5a1
79538 <xxxxxxxxxx.123@mail.ru>
2023-06-06 21:14:16 +0300

    homework-2_completed

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
 
 
@@ -22,3 +24,29 @@ def test_apply_discount():
     test_item4.price = test_item4.price * Item.pay_rate
 
     assert test_item4.price == 50000.0
+
+def test_name():
+    Item.name = 'Игра-Кот'
+    assert Item.name == 'Игра-Кот'
+
+@pytest.fixture
+def item():
+    Item.all = []
+    item = Item(name="Игра-Динозавр", price=1200, quantity=25)
+    return item
+
+def test_all_items_list(item):
+    assert len(Item.all) == 1
+    assert Item.all[0] == item
+
+def test_len_name(item):
+    with pytest.raises(Exception) as e:
+        item.name = "Игра-Динозавр"
+        assert str(e.value) == "Длина наименования товара превышает 10 символов"
+def test_len_name2(item):
+    item.name = "Телевизор"
+    assert item.name == "Телевизор"
+def test_instantiate_from_csv(item):
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    assert isinstance(Item.all[0], Item)
